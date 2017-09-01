990e74699286691b6aeeef19c442829d7854767e
Andrea Lai <laidevpub@gmail.com>
2017-09-01 13:10:16 +0200

    mix.exs add distillery

--- mix.exs
+++ mix.exs
@@ -36,7 +36,9 @@ defmodule Phgeox.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:corsica, "~> 1.0"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:distillery, "~> 1.5"}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
